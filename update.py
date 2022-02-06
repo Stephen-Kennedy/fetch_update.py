bec45e1a3f6bd018163b4f002e52a98b1c4aac97
Stephen <medicup@gmail.com>
2022-02-06 06:43:17 -0500

    Update update.py
    Updated auto_update() to include autoremove

--- update.py
+++ update.py
@@ -11,11 +11,11 @@ logger = logging.getLogger()
 logger.addHandler(SysLogHandler('/dev/log'))
 logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
 
+# Checks respositories for available updates, installs upgrades, removes old packages,
+# clears local repository of packages that are no longer useful
 def auto_update():
-  updates = ['update', 'upgrade', 'remove', 'autoclean']
+  updates = ['update', 'upgrade', 'remove', 'autoclean', 'autoremove']
 
-  # Checks respositories for available updates, installs upgrades, removes old packages,
-  # clears local repository of packages that are no longer useful
   for update in updates:
     os.system('apt -y %s' % (update))
     logger.warning('Running apt %s.' % (update))
