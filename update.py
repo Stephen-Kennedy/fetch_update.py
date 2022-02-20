ab5e98017be46da00378e0206d25b2f545c47b27
Stephen <medicup@gmail.com>
2022-02-20 12:34:19 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
-# Author Stephen J Kennedy
-# Version 1.0
-# Auto update script for updating debian/ubuntu with Python
+# update.py version to auto-update, to include pihole, via cronjob
 import os
-import time
 from logging.handlers import SysLogHandler
 import logging
 
@@ -11,24 +7,30 @@ logger = logging.getLogger()
 logger.addHandler(SysLogHandler('/dev/log'))
 logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
 
-# Checks respositories for available updates, installs upgrades, removes old packages,
-# clears local repository of packages that are no longer useful
 def auto_update():
-  updates = ['update', 'upgrade', 'remove', 'autoclean', 'autoremove']
+    updates = ['update', 'upgrade', 'remove', 'autoclean']
 
-  for update in updates:
-    os.system('apt -y %s' % (update))
-    logger.warning('Running apt %s.' % (update))
+    # Checks respositories for available updates, installs upgrades, removes old packages,
+    # clears local repository of packages that are no longer useful
+    for update in updates:
+        os.system('apt -y %s' % (update))
+        logger.warning('Running apt %s.' % (update))
+
+# checks for update to pihole
+def piupdate ():
+    os.system('pihole -up')
+    logger.warning('Running pihole update.')
 
 # Checks to see if "reboot-required" file exists in /var/run/.
 def auto_restart():
-  reboot_exists = ""
-  if os.path.isfile('/var/run/reboot-required') == True:
-     logging.warning('Reboot required. Rebooting now')
-     os.system('shutdown -r now')
+    reboot_exists = ""
+    if os.path.isfile('/var/run/reboot-required') == True:
+        logger.warning("*** REBOOT REQUIRED / REBOOTING NOW ***")
+        os.system('shutdown -r now')
 
-  else:
-      logging.warning('No reboot required. Update complete.')
+    else:
+        logger.warning('No reboot required. Update complete.')
 
 auto_update()
+piupdate()
 auto_restart()
