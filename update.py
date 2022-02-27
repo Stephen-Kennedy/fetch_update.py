a3f92858c2fe38efc8641dc5a70029fe090da491
Stephen <medicup@gmail.com>
2022-02-27 16:57:14 -0500

    Update update.py
    converted to Format strings as opposed to %s.

--- update.py
+++ update.py
@@ -13,8 +13,8 @@ def auto_update():
     # Checks respositories for available updates, installs upgrades, removes old packages,
     # clears local repository of packages that are no longer useful
     for update in updates:
-        os.system('apt -y %s' % (update))
-        logger.warning('Running apt %s.' % (update))
+        os.system(f'apt -y {update}')
+        logger.warning(f'Running apt {update}')
 
 # checks for update to pihole
 def piupdate ():
@@ -23,13 +23,13 @@ def piupdate ():
 
 # Checks to see if "reboot-required" file exists in /var/run/.
 def auto_restart():
-    reboot_exists = ""
+    #reboot_exists = ""
     if os.path.isfile('/var/run/reboot-required') == True:
-        logger.warning("*** REBOOT REQUIRED / REBOOTING NOW ***")
+        logger.warning("*** REBOOT REQUIRED ***")
         os.system('shutdown -r now')
 
     else:
-        logger.warning('No reboot required. Update complete.')
+        logger.warning(' Update complete.')
 
 auto_update()
 piupdate()
