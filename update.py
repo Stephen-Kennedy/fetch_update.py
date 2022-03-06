d3d192dd84dd73037005ae0e2eef5e1fa62662a6
Stephen <medicup@gmail.com>
2022-03-06 11:47:08 -0500

    Update update.py
    updated to add socket.gethostname() for improved logging.

--- update.py
+++ update.py
@@ -6,10 +6,14 @@ import os
 import time
 from logging.handlers import SysLogHandler
 import logging
+import socket
 
 logger = logging.getLogger()
 logger.addHandler(SysLogHandler('/dev/log'))
 logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
+
+# get local machine hostname
+local_host = socket.gethostname()
 
 # Checks respositories for available updates, installs upgrades, removes old packages,
 # clears local repository of packages that are no longer useful
@@ -17,18 +21,18 @@ def auto_update():
   updates = ['update', 'upgrade', 'remove', 'autoclean', 'autoremove']
 
   for update in updates:
-    os.system('apt -y %s' % (update))
-    logger.warning('Running apt %s.' % (update))
+    os.system(f'apt -y {update}')
+    logger.warning(f'Running apt {update} on {local_host}.')
 
 # Checks to see if "reboot-required" file exists in /var/run/.
 def auto_restart():
   reboot_exists = ""
   if os.path.isfile('/var/run/reboot-required') == True:
-     logging.warning('***REBOOT required. Rebooting now***')
+     logging.warning(f'***REBOOT required. Rebooting {local_host} now***')
      os.system('shutdown -r now')
 
   else:
-      logging.warning('   Update complete.')
+      logging.warning(f'   Update complete on {local_host}.')
 
 auto_update()
 auto_restart()
