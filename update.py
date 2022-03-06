0903414edcd6069dcefae9a368fc61b5414db172
Stephen <medicup@gmail.com>
2022-03-06 11:39:21 -0500

    Update update.py
    converted get hostname from os.system to socket.gethostname() for improved logging.

--- update.py
+++ update.py
@@ -1,12 +1,16 @@
-# update.py version to auto-update, to include pihole, via cronjob
+# update.py version to auto-update, to include pihole, via cronjob 
 import os
 from logging.handlers import SysLogHandler
 import logging
+import socket
 
 logger = logging.getLogger()
 logger.addHandler(SysLogHandler('/dev/log'))
 logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
 
+# get current hostname
+current_host = socket.gethostname()
+    
 def auto_update():
     updates = ['update', 'upgrade', 'remove', 'autoclean']
 
@@ -19,15 +23,13 @@ def auto_update():
 # checks for update to pihole
 def piupdate ():
     os.system('pihole -up')
-    logger.warning('Running pihole update.')
+    logger.warning(f'Running pihole update on {current_host}. ')
 
 # Checks to see if "reboot-required" file exists in /var/run/.
 def auto_restart():
-    # get local_host name
-    get_hostname = os.system('hostname')
-    
-    if os.path.isfile('/var/run/reboot-required') == True:
-        logger.warning(f"*** REBOOT REQUIRED for host: {get_hostname}. Rebooting now ***")
+   # if os.path.isfile('/var/run/reboot-required') == True:
+    if os.path.isfile('/var/run/reboot-requred') == True:
+        logger.warning(f"*** REBOOT REQUIRED for host: {current_host}. Rebooting now ***")
         os.system('shutdown -r now')
 
     else:
