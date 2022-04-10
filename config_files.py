00df2154a271bc7f86b812d6db3c13573a5e52c1
Stephen <medicup@gmail.com>
2022-04-10 12:34:53 -0400

    Update config_files.py

--- config_files.py
+++ config_files.py
@@ -15,9 +15,18 @@ logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
 github_path = 'https://raw.githubusercontent.com/Stephen-Kennedy/fetch_update/master/'
 current_directory = os.getcwd()
 
+# get current custom file list from Github
+file_list_name = 'filelist.txt'
+os.system(f'wget {github_path}/{file_list_name}')
+
+# custom files to maintain
+get_my_file_list = open(f'{current_directory}/{file_list_name}','r')
+set_file_list = get_my_file_list.split("\n")
+get_my_file_list.close()
+                    
 def custom_files_to_update():
-    # custom files to maintain
-    custom_files = ['/usr/local/bin/update.py']
+
+                        
 
     """ Checks to see if file from custom_file exists on current system. If so, downloads
     current Github version and compares current file to newly downloaded version.  If the files
