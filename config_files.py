e5a47247c09e8f24305de0356b92ac6c662a290c
Stephen <medicup@gmail.com>
2024-05-11 22:54:06 -0400

    Revised for efficiency and security

--- config_files.py
+++ config_files.py
@@ -1,59 +1,63 @@
 #!/usr/bin/python3
 # Author Stephen J Kennedy
-# Version 1.0
-# Script to check for and update most current installs
+# Version 1.1
+# Script to check for and update the most current installs
+
 import filecmp
 import logging
 import os
-import socket
+import subprocess
 from logging.handlers import SysLogHandler
+from tempfile import NamedTemporaryFile
 
+# Configure Logging
 logger = logging.getLogger()
-logger.addHandler(SysLogHandler('/dev/log'))
-logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
+logger.setLevel(logging.INFO)
+handler = SysLogHandler('/dev/log')
+file_handler = logging.FileHandler('/var/log/pyupdate.log')
+logger.addHandler(handler)
+logger.addHandler(file_handler)
 
 github_path = 'https://raw.githubusercontent.com/Stephen-Kennedy/fetch_update/master/'
 current_directory = os.getcwd()
 
 
+def run_command(command):
+    """ Utility function to run a shell command. """
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Command failed: {e}")
+        return None
+
+
 def get_update_list():
-    """ Get the most current list file from the Github fetch_update master. Read the txt file
-    into a list for custom_files_to_update(). After processing custom_file_to_update() remove
-    temp_file_list"""
-
+    """ Get the most current list file from the GitHub fetch_update master and update files. """
     file_list_name = 'file_list.txt'
-    get_file_list = (f'{github_path}{file_list_name}')
-    os.system(f'wget {get_file_list}')
+    file_list_url = f'{github_path}{file_list_name}'
 
-    temp_file_list = (f'{current_directory}/{file_list_name}')
-    with open(f'{temp_file_list}') as file:
-        file_list = file.read().splitlines()
+    with NamedTemporaryFile(delete=False) as tmp_file:
+        run_command(f'wget -O {tmp_file.name} {file_list_url}')
+        with open(tmp_file.name, 'r') as file:
+            file_list = file.read().splitlines()
+        os.unlink(tmp_file.name)
 
     custom_files_to_update(file_list)
-    os.system(f'rm {temp_file_list}.*')
 
 
 def custom_files_to_update(files):
-    """ Checks to see if file from custom_file exists on current system. If so, downloads
-    current Github version and compares current file to newly downloaded version.  If the files
-    do not match, replace existing file with new version. """
-
+    """ Checks and updates files based on the list from GitHub. """
     for file in files:
-        get_file_name = os.path.basename(file)
-        os.system(f'wget {github_path}{get_file_name}')
-        existing_file = (f'{file}')
-        new_file = (f'{current_directory}/{get_file_name}')
+        file_name = os.path.basename(file)
+        new_file_path = os.path.join(current_directory, file_name)
 
-        if os.path.isfile(file):
-            existing_file = (f'{file}')
-
-            if not filecmp.cmp(existing_file, new_file):
-                os.system(f'cp {new_file} {existing_file}')
-                logger.warning(f'*** New file configurations on Github. ***')
-        else:
-            os.system(f'cp {new_file} {file}')
-
-        os.system(f'rm {new_file}')
+        if run_command(f'wget -O {new_file_path} {github_path}{file_name}') is not None:
+            if os.path.isfile(file) and not filecmp.cmp(file, new_file_path):
+                run_command(f'cp {new_file_path} {file}')
+                logger.info(f'Updated file from GitHub: {file}')
+            os.unlink(new_file_path)
 
 
 get_update_list()
--- update.py
+++ update.py
@@ -1,49 +1,62 @@
 #!/usr/bin/python3
 # Author Stephen J Kennedy
-# Version 2.0
+# Version 2.1
 # Auto update script for updating debian/ubuntu with Python
+
+import subprocess
 import os
-import time
-from logging.handlers import SysLogHandler
 import logging
-import socket
+from logging.handlers import SysLogHandler
 
-logger = logging.getLogger()
-logger.addHandler(SysLogHandler('/dev/log'))
-logger.addHandler(logging.FileHandler('/var/log/pyupdate.log'))
+# Configure Logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('/var/log/pyupdate.log')
+file_handler.setFormatter(formatter)
+syslog_handler = SysLogHandler(address='/dev/log')
+syslog_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(syslog_handler)
 
-host_name = socket.gethostname()
+host_name = os.uname()[1]
 
+def run_command(command):
+    """ Run shell command and log output. """
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        logger.info(result.stdout)
+        if result.stderr:
+            logger.error(result.stderr)
+        return result.returncode == 0
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Command failed: {e}")
+        return False
 
-# Checks respositories for available updates, installs upgrades, removes old packages,
-# clears local repository of packages that are no longer useful
 def auto_update():
-    updates = ['update', 'upgrade', 'remove', 'autoclean', 'autoremove']
+    """ Performs system updates and cleans up. """
+    updates = ['update', 'upgrade', 'autoremove', 'autoclean']
 
     for update in updates:
-        os.system(f'apt -y {update}')
-        logger.warning(f'Running apt {update} on {host_name}.')
+        if run_command(f'apt-get -y {update}'):
+            logger.info(f'Successfully ran apt-get {update} on {host_name}')
+        else:
+            logger.error(f'Failed to run apt-get {update} on {host_name}')
 
     if os.path.isfile('/usr/local/bin/pihole'):
-        piupdate()
+        if run_command('pihole -up'):
+            logger.info('Successfully updated pihole.')
+        else:
+            logger.error('Failed to update pihole.')
 
-
-# checks for update to pihole
-def piupdate():
-    os.system('pihole -up')
-    logger.warning(f'Running pihole update on {host_name}. ')
-
-
-# Checks to see if "reboot-required" file exists in /var/run/.
 def auto_restart():
-    reboot_exists = ""
-    if os.path.isfile('/var/run/reboot-required') == True:
+    """ Checks if a reboot is required and performs it. """
+    if os.path.isfile('/var/run/reboot-required'):
         logger.warning(f"**** REBOOT REQUIRED for host: {host_name}. Rebooting now ****")
-        os.system('reboot')
-
+        run_command('reboot')
     else:
-        logging.warning(f'**** Update complete on {host_name}.')
+        logger.info(f'Update complete on {host_name}. No reboot required.')
 
-
-auto_update()
-auto_restart()
+if __name__ == "__main__":
+    auto_update()
+    auto_restart()
